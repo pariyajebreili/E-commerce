c1bcd3cb482c7275e13d7465da08f72cb4dc75ba
Pariya <pariyajebreili@gmail.com>
2022-12-22 01:26:55 +0330

    new changes

---
+++ shop/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from store.models import Product
+
+def home(request):
+
+    products = Product.objects.all().filter(is_available=True).order_by('created_date')
+
+    context = {
+        'products': products,
+    }
+    return render(request, 'home.html', context)
+ 
